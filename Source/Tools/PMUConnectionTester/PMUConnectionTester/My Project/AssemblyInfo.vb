7d39173ba787f5dbfd5653643418a890491d47cf
pinalcpatel <pcpatel@tva.gov>
2011-03-06 05:01:34 +0000

    PMUConnectionTester: Version change for build PMUConnectionTester_v4.2.6.0.
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS02]$/pmuconnectiontester;C69800

--- Source/Tools/PMUConnectionTester/PMUConnectionTester/My Project/AssemblyInfo.vb
+++ Source/Tools/PMUConnectionTester/PMUConnectionTester/My Project/AssemblyInfo.vb
@@ -2,7 +2,7 @@
 Imports System.Runtime.InteropServices
 
 ' Assembly identity attributes.
-<Assembly: AssemblyVersion("4.2.5.0")> 
+<Assembly: AssemblyVersion("4.2.6.0")> 
 
 ' Informational attributes.
 <Assembly: AssemblyCompany("Grid Protection Alliance")> 
