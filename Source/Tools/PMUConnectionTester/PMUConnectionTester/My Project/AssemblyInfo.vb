b4749e58568cc8aab28b97f48b6693977a14e48a
ritchiecarroll <rcarroll@gridprotectionalliance.org>
2013-07-16 12:45:54 +0000

    Updated version to 4.4.0 - version includes updated dependencies for latest synchrophasor protocols associated with the production release of 1.5 SP1 of the openPDC - this includes all bug fixes to protocols from Dec 2012 until now including fixes for F-NET, BPA PDCstream, Macrodyne and various DFR's/PQ meters.
    git-tfs-id: [https://tfs.codeplex.com:443/tfs/TFS02]$/pmuconnectiontester;C101795

--- Source/Tools/PMUConnectionTester/PMUConnectionTester/My Project/AssemblyInfo.vb
+++ Source/Tools/PMUConnectionTester/PMUConnectionTester/My Project/AssemblyInfo.vb
@@ -2,7 +2,7 @@
 Imports System.Runtime.InteropServices
 
 ' Assembly identity attributes.
-<Assembly: AssemblyVersion("4.3.11.0")> 
+<Assembly: AssemblyVersion("4.4.0.0")> 
 
 ' Informational attributes.
 <Assembly: AssemblyCompany("Grid Protection Alliance")> 
