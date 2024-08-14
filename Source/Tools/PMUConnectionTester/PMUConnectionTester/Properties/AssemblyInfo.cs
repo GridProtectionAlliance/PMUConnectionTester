c273eb6da3ab2bd513a6d0f8c2349a9e0cf5597f
gsfbuildbot <support@gridprotectionalliance.org>
2024-08-14 15:51:44 -0400

    PMUConnectionTester: Version change for build v4.7.3.0-master.

--- Source/Tools/PMUConnectionTester/PMUConnectionTester/Properties/AssemblyInfo.cs
+++ Source/Tools/PMUConnectionTester/PMUConnectionTester/Properties/AssemblyInfo.cs
@@ -2,7 +2,7 @@
 using System.Runtime.InteropServices;
 
 // Assembly identity attributes.
-[assembly: AssemblyVersion("4.7.2.0")]
+[assembly: AssemblyVersion("4.7.3.0")]
 
 // Informational attributes.
 [assembly: AssemblyCompany("Grid Protection Alliance")]
